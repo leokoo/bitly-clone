be5e506f1aa82924a758e050af21e9d7c4151431
Leo Koo <leokoo@gmail.com>
2015-11-26 09:05:02 +0800

    counter for clicks works, thanks to SSY

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 get '/' do
 	@urls = Url.all
   erb :"static/index"
@@ -11,10 +13,18 @@ end
 
 # i.e. /q6bda
 get '/:short_url' do
-	url = Url.find_by(shortened_url: params[:short_url])
+	@url = Url.find_by(shortened_url: params[:short_url])
+	# byebug
+	# increase counter
+	# i = Url.find_by(counter: params[:short_url])
+	# @url.counter +=1
+	# byebug
+	# i += 1
 	# redirect to appropriate "long" URL
-	unless url.nil?
-		redirect url.long_url
+	unless @url.nil? # if not nil
+		@url.counter +=1
+		@url.save
+		redirect @url.long_url	
 	else
 		redirect "/"
 	end
